1dc3d562174e05c6c57eacbf1182f2aa82a7c894
Bostjan Zupan <bostjanzup@gmail.com>
2023-12-21 13:26:31 +0100

    load config with viper

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/NightShop/fasunga-project/api"
 	db "github.com/NightShop/fasunga-project/db/sqlc"
+	"github.com/NightShop/fasunga-project/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/fasunga?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot laod config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
